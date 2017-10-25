d94318dff037f2bdc67d883e1eb065e067376956
Старовойтов Антон Станиславович <AStarovoytov@alfabank.ru>
2017-10-25 15:50:27 +0500

    Реализованы проверки и подсчет очков

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
 # Password Strength Calculator
 
-[TODO. There will be project description]
+This module checks strength of password
+
+# Usage Examples
+
+```bash
+
+$ python password_strength.py asdasd
+Weak password
+
+$ python password_strength.py admin
+Your password is in password blacklist
+Very weak password
+
+$ python password_strength.py asd12SA
+Strong password
+```
 
 # Project Goals
 
--- password_strength.py
+++ password_strength.py
@@ -1,8 +1,32 @@
+import sys
+import re
 
 
 def get_password_strength(password):
-    pass
+    strength_points = 0
+    password_blacklist = open('./pass.txt', 'r').read().split('\n')
+    if re.search(r"\d", password):
+        strength_points += 2
+    if re.search(r"[A-Z]", password) and re.search(r"[a-z]", password):
+        strength_points += 4
+    if re.search(r"[@,#,$]", password):
+        strength_points += 2
+    if password in password_blacklist:
+        strength_points = 1
+        print('Your password is in password blacklist')
+    else:
+        strength_points += 2
+
+    if strength_points == 1:
+        print('Very weak password')
+    elif 1 < strength_points < 5:
+        print('Weak password')
+    elif 5 < strength_points < 10:
+        print('Strong password')
+    elif strength_points == 10:
+        print('Very cool password')
 
 
 if __name__ == '__main__':
-    pass
+    password = sys.argv[1]
+    get_password_strength(password)
