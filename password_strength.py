7767a4d94094f49a385f90d85403ad257d6ef8b7
Старовойтов Антон Станиславович <AStarovoytov@alfabank.ru>
2017-10-25 17:09:24 +0500

    удалил лишнее

--- password_strength.py
+++ password_strength.py
@@ -2,8 +2,6 @@ import sys
 import re
 from string import punctuation
 
-strength_points = 0
-
 
 def load_passwords_black_list(filepath='./pass.txt'):
     with open(filepath, 'r') as black_list_passwords:
@@ -12,22 +10,22 @@ def load_passwords_black_list(filepath='./pass.txt'):
 
 def password_strength_check(password):
     password_blacklist = load_passwords_black_list()
-    global strength_points
+    strength_points = 0
+
     if re.search(r"\d", password):
         strength_points += 2
-    if re.search(r"{}".format(punctuation), password) and re.search(r"[a-z]",
-                                                                    password):
-        strength_points += 4
-    if re.search(r"[@,#,$]", password):
+    if re.search(r"[a-z]",password) and re.search(r"[A-Z]", password):
         strength_points += 2
+    if re.search(r"[{}]".format(punctuation), password):
+        strength_points += 4
     if password in password_blacklist:
         strength_points = 1
-        print('Your password is in password blacklist')
     else:
         strength_points += 2
+    return strength_points
 
 
-def get_password_strength():
+def get_password_strength(strength_points):
     if strength_points == 1:
         print('Very weak password')
     elif 1 < strength_points < 5:
@@ -40,5 +38,5 @@ def get_password_strength():
 
 if __name__ == '__main__':
     password = sys.argv[1]
-    password_strength_check(password)
-    get_password_strength()
+    strength_points = password_strength_check(password)
+    get_password_strength(strength_points)
